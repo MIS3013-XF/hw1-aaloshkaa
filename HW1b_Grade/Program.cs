0762635a5f492aad9f6cd90dfb2d0039f99bac55
Khrais, Ally M. <allykhrais@ou.edu>
2025-09-10 10:33:37 -0500

    Console added Grade

--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1b Grade
 
-// Your Name:
+// Your Name:Ally Khrais
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -15,6 +15,36 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your first name?");
+            String firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            String lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your student ID number?");
+            String studentID = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for homework?");
+            Double homeworkGrade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for participation?");
+            Double participationGrade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            Double quizzesGrade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for the midterm?");
+            Double midtermGrade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is your overall percentage grade for the final?");
+            Double finalGrade = Convert.ToDouble(Console.ReadLine());
+
+            //calculate the grade according to the syllabus 
+
+            Double overallGrade = 0;
+
+            Console.WriteLine(firstName + " " + lastName + " your overall grade is " + overallGrade);
+
         }
     }
 }
